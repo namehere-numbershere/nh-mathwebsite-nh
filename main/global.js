04b664762aba1616309bc9235d6380439d03b2cf
NamehereNumbersHere <111458916+namehere-numbershere@users.noreply.github.com>
2024-08-23 19:41:10 -0400

    Update global.js

--- main/global.js
+++ main/global.js
@@ -1,6 +1,11 @@
-console.log("Global script initialized!");
+const env = {
+    env_variable: "bgrtyaqQtyfadV0F08BHGvfsgskl",
+    topic_id: "F08BHGvfsgsklgrtyaqQtyfadV0F08"
+}
 
+console.log(process.env.env_variable)
 
+// prints bgrtyaqQtyfadV0F08BHGvfsgskl to the console
 
 
 
